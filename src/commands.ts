5aeeb7b03169e0be227594b60041c0f513fd64a8
ow-- <ow--@users.noreply.github.com>
2016-01-14 23:58:16 +0100

    Scroll the editor to keep the cursor visible (fixes #5)

--- src/commands.ts
+++ src/commands.ts
@@ -1,24 +1,28 @@
-import { Selection, TextEditor, TextEditorEdit } from 'vscode';
+import { Selection, TextEditor, TextEditorEdit, TextEditorRevealType } from 'vscode';
 import { nextBoundaryLeft, nextBoundaryRight } from './boundaries';
 
 export function cursorSubwordLeft(editor: TextEditor) {
     const pos = nextBoundaryLeft(editor.document, editor.selection.active);
     editor.selection = new Selection(pos, pos);
+    editor.revealRange(editor.selection, TextEditorRevealType.InCenterIfOutsideViewport);
 }
 
 export function cursorSubwordRight(editor: TextEditor) {
     const pos = nextBoundaryRight(editor.document, editor.selection.active);
     editor.selection = new Selection(pos, pos);
+    editor.revealRange(editor.selection, TextEditorRevealType.InCenterIfOutsideViewport);
 }
 
 export function cursorSubwordLeftSelect(editor: TextEditor) {
     const pos = nextBoundaryLeft(editor.document, editor.selection.active);
     editor.selection = new Selection(editor.selection.anchor, pos);
+    editor.revealRange(editor.selection, TextEditorRevealType.InCenterIfOutsideViewport);
 }
 
 export function cursorSubwordRightSelect(editor: TextEditor) {
     const pos = nextBoundaryRight(editor.document, editor.selection.active);
     editor.selection = new Selection(editor.selection.anchor, pos);
+    editor.revealRange(editor.selection, TextEditorRevealType.InCenterIfOutsideViewport);
 }
 
 export function deleteSubwordLeft(editor: TextEditor, edit: TextEditorEdit) {
@@ -28,6 +32,7 @@ export function deleteSubwordLeft(editor: TextEditor, edit: TextEditorEdit) {
     }
     const pos = nextBoundaryLeft(editor.document, editor.selection.active);
     edit.delete(editor.selection.with(pos));
+    editor.revealRange(editor.selection, TextEditorRevealType.InCenterIfOutsideViewport);
 }
 
 export function deleteSubwordRight(editor: TextEditor, edit: TextEditorEdit) {
@@ -37,4 +42,5 @@ export function deleteSubwordRight(editor: TextEditor, edit: TextEditorEdit) {
     }
     const pos = nextBoundaryRight(editor.document, editor.selection.active);
     edit.delete(editor.selection.with(pos));
+    editor.revealRange(editor.selection, TextEditorRevealType.InCenterIfOutsideViewport);
 }
