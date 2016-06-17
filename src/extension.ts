808f01f528291852ce8ffe2d0b83e0104da3174a
massimocode <info@massimoahmadi.com>
2016-06-17 17:06:42 +0100

    Stop navigation needlessly entering editor history

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,28 @@
-import { commands } from 'vscode';
+import { commands, window } from 'vscode';
 import * as subwordNavigation from './commands';
 
 export function activate() {
-    commands.registerTextEditorCommand(
+    commands.registerCommand(
         'subwordNavigation.cursorSubwordLeft',
-        subwordNavigation.cursorSubwordLeft);
+        () => subwordNavigation.cursorSubwordLeft(window.activeTextEditor));
 
-    commands.registerTextEditorCommand(
+    commands.registerCommand(
         'subwordNavigation.cursorSubwordRight',
-        subwordNavigation.cursorSubwordRight);
+        () => subwordNavigation.cursorSubwordRight(window.activeTextEditor));
 
-    commands.registerTextEditorCommand(
+    commands.registerCommand(
         'subwordNavigation.cursorSubwordLeftSelect',
-        subwordNavigation.cursorSubwordLeftSelect);
+        () => subwordNavigation.cursorSubwordLeftSelect(window.activeTextEditor));
 
-    commands.registerTextEditorCommand(
+    commands.registerCommand(
         'subwordNavigation.cursorSubwordRightSelect',
-        subwordNavigation.cursorSubwordRightSelect);
+        () => subwordNavigation.cursorSubwordRightSelect(window.activeTextEditor));
 
-    commands.registerTextEditorCommand(
+    commands.registerCommand(
         'subwordNavigation.deleteSubwordLeft',
-        subwordNavigation.deleteSubwordLeft);
+        () => subwordNavigation.deleteSubwordLeft(window.activeTextEditor));
 
-    commands.registerTextEditorCommand(
+    commands.registerCommand(
         'subwordNavigation.deleteSubwordRight',
-        subwordNavigation.deleteSubwordRight);
+        () => subwordNavigation.deleteSubwordRight(window.activeTextEditor));
 }
