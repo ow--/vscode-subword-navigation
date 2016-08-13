578520a5059a048b53f9de496eb10db04b937ffa
ow-- <ow--@users.noreply.github.com>
2016-08-13 15:17:55 +0200

    Check line range in both directions (fixes #13)

--- src/boundaries.ts
+++ src/boundaries.ts
@@ -6,9 +6,9 @@ const separators = /[\`\~\!\@\#\$\%\^\&\*\(\)\-\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\
 export function nextBoundaryLeft(document: TextDocument, position: Position) {
     // find previous word boundary
     const cur = document.lineAt(position);
-    if (position.character > 0) {
+    if (position.isAfter(cur.range.start)) {
         for (let i = position.translate(0, -1); i.isAfterOrEqual(cur.range.start); i = i.translate(0, -1)) {
-            if (i.character < 1) return i;
+            if (i.isEqual(cur.range.start)) return i;
             if (isBoundary(cur.text, i)) return i;
         }
     }
@@ -26,11 +26,12 @@ export function nextBoundaryRight(document: TextDocument, position: Position) {
     // find next word boundary
     const cur = document.lineAt(position);
     for (let i = position.translate(0, 1); i.isBeforeOrEqual(cur.range.end); i = i.translate(0, 1)) {
+        if (i.isEqual(cur.range.end)) return i;
         if (isBoundary(cur.text, i)) return i;
     }
-    
+
     // found no boundary before line end
-    if (position.line + 1 >= document.lineCount) return cur.range.end;    
+    if (position.line + 1 >= document.lineCount) return cur.range.end;
     const next = document.lineAt(position.line + 1);
     return next.range.start.translate(0, next.firstNonWhitespaceCharacterIndex);
 }
@@ -39,7 +40,7 @@ function isBoundary(text: string, position: Position) {
     const prev = char(text[position.character - 1]);
     const cur = char(text[position.character]);
     const next = char(text[position.character + 1]);
-    
+
     if (prev.separator !== cur.separator) return true;
     if (cur.underscore && !prev.underscore) return true;
     if (prev.underscore && !cur.underscore) return true;
@@ -47,20 +48,20 @@ function isBoundary(text: string, position: Position) {
     if (prev.numeric && !cur.numeric) return true;
     if (cur.upper && prev.lower) return true;
     if (cur.upper && next.lower) return true;
-    
+
     return false;
 }
 
 function char(c: string) {
     const cl = { none: false, upper: false, lower: false, numeric: false, underscore: false, separator: "" };
-    
+
     if (!c) cl.none = true;
     else if (c === '_') cl.underscore = true;
     else if (isSeparator(c)) cl.separator = c;
     else if (isDigit(c)) cl.numeric = true;
     else if (isUpper(c)) cl.upper = true;
     else if (isLower(c)) cl.lower = true;
-    
+
     return cl;
 }
 
